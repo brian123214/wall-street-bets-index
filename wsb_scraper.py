e583ded310d3688e35a3427d0243d00650180880
brian654321 <58054213+brian654321@users.noreply.github.com>
2020-12-12 20:09:46 -0500

    Update wsb_scraper.py

--- wsb_scraper.py
+++ wsb_scraper.py
@@ -2,95 +2,65 @@ import praw
 from praw.models import MoreComments
 import matplotlib.pyplot as plt
 
-# Gets the top 100 common english words since some tickers have the same name as common words
-def get_common():
-    fin = open("common.txt", 'r')
-    words = set()
-    for x in fin.readlines():
-        words.add(x[:-1])
-    return words
-    
-# Creates set of tickers
+# Creates a set of stock tickers in NASDAQ
 def get_tickers():
     fin = open("nasdaqtraded.txt", 'r')
     tickers = set()
-    temp = "0"
     fin.readline()
-    while temp != "" and temp[0] != "F":
-        temp = fin.readline().strip()
-        try:
-            temp1 = (temp[2:temp.index("|", 3)]).upper()
-            tickers.add(temp1)
-        except:
-            pass
+    for line in fin.readlines():
+        line = line[2:]
+        tickers.add(line[:line.index("|")])
     return tickers
-    
+
+# Iterates through only head comments 
 def iter_top_level(comments):
     for top_level_comment in comments:
         if isinstance(top_level_comment, MoreComments):
             yield from iter_top_level(top_level_comment.comments())
         else:
             yield top_level_comment
-            
 
 def get_data():
-    reddit = praw.Reddit(client_id = ENTER YOUR OWN, client_secret = ENTER YOUR OWN, user_agent = ENTER YOUR OWN)
+    reddit = praw.Reddit(client_id = "ENTER OWN", client_secret = "ENTER OWN", user_agent = "ENTER OWN")
     counter = 0
-    common_words = get_common()
+    # People may use use words that happen to be real ticker names
+    flagged_words = ["YOLO", "PUMP", "RH", "EOD", "IPO", "ATH", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", 
+        "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
     ticker_set = get_tickers()
-    tickers = dict()
-
+    tickers = {}
     sub = reddit.subreddit("wallstreetbets")
     for submission in sub.search('flair:"Daily Discussion"', limit = 1):
         print(submission.title)
         for comment in iter_top_level(submission.comments): 
-            # set counter to how many comments you want to search
-            if counter == 400:
+            # set how many comments you want to search
+            if counter == 1000:
                 return tickers
-            comment_set = set(comment.body.upper().split())
-            similar = comment_set.intersection(ticker_set)
-            for word in similar:
-                if word.lower() in common_words:
-                    continue
-                if word not in tickers:
-                    tickers[word] = 1
-                else:
-                    tickers[word] += 1
+            for word in comment.body.split():
+                if word == word.upper() and word in ticker_set and word not in flagged_words:
+                    if word not in tickers:
+                        tickers[word] = 1
+                    else:
+                        tickers[word] += 1
+            counter += 1
             counter += 1
     return tickers
 
-def get_mentioned_tickers():
+def popularTickers():
     result = get_data()
+    print(result)
     x = []
     y = []
-    
-    # Threshold is how many mentions you want for a ticker to be considered
-    threshold = 5
     for a, b in result.items():
-    
-        if b > threshold:
+        # Can change value to see choose the threshold stock mention count 
+        if b > 0:
             x.append(a)
             y.append(b)
-            
-    final_x = x
-    final_y = y
-    
-    # Can include the following if you want to prune the tickers yourself to check if there are tickers that are actually just a common word in there
-    # for j in range(len(x)):
-    #     user = input("Is " + x[j] + " an actual ticker? t or f (some tickers share names with common words)")
-    #     if user == "t":
-    #         final_x.append(x[j])
-    #         final_y.append(y[j])
-    
-    # Can include if you want to see a bar graph
-    # x_pos = [i for i, _ in enumerate(final_x)]
-    # plt.bar(x_pos, final_y, color='blue')
+    # If you want to see a plot of the number of mentioned tickers
+    # x_pos = [i for i, _ in enumerate(x)]
+    # plt.bar(x_pos, y, color='blue')
     # plt.xlabel("Tickers")
     # plt.ylabel("Mentions")
     # plt.title("Wall Street Bets Index")
-    # plt.xticks(x_pos, final_x)
+    # plt.xticks(x_pos, x)
     # plt.show()
-    
-    return final_x, final_y
-
-
+    return x, y
